f157f2d5b49b7217cc5b88998195ba5c11928fbf
Danila Vershinin <ciapnz@gmail.com>
2024-06-30 12:17:58 +0500

    Move block-ip.sh and refactor IP checks
    The block-ip.sh script has been moved from the libexec directory to the sbin directory. Additionally, the code checking for trusted IPs has been refactored from a single line grep operation to a more readable 'for' loop. Corrected handling of the IP variable in string format prevents potential errors in script execution.

--- sbin/block-ip.sh
+++ sbin/block-ip.sh
@@ -10,18 +10,20 @@ if [[ -z ${REMOTE_ADDR} ]]; then
 fi
 
 # Put space separate list of trusted IP addresses, not to lock yourself out if you like to test the honeypot! : )
-TRUSTED_IPS=(1.2.3.4 2.3.4.5 127.0.0.1)
-if printf '%s\n' ${TRUSTED_IPS[@]} | grep -q -P "^$REMOTE_ADDR\$"; then
-    echo "Trusted IP"
-    exit 0
-fi
+TRUSTED_IPS=("1.2.3.4" "2.3.4.5" "127.0.0.1")
+for ip in "${TRUSTED_IPS[@]}"; do
+    if [[ "$REMOTE_ADDR" == "$ip" ]]; then
+        echo "Trusted IP"
+        exit 0
+    fi
+done
 
 if [[ "$REMOTE_ADDR" != "${1#*[0-9].[0-9]}" ]]; then
-  /sbin/ipset add honeypot4 ${REMOTE_ADDR}
-  /sbin/conntrack -D -s ${REMOTE_ADDR}
+  /sbin/ipset add honeypot4 "${REMOTE_ADDR}"
+  /sbin/conntrack -D -s "${REMOTE_ADDR}"
 elif [[ "$REMOTE_ADDR" != "${1#*:[0-9a-fA-F]}" ]]; then
-  /sbin/ipset add honeypot6 ${REMOTE_ADDR}
-  /sbin/conntrack -D -s ${REMOTE_ADDR}
+  /sbin/ipset add honeypot6 "${REMOTE_ADDR}"
+  /sbin/conntrack -D -s "${REMOTE_ADDR}"
 else
   echo "Unrecognized IP format '$1'"
 fi
