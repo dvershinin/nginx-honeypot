df417ae69659ffeefec0f2739137b713d8850d03
Danila Vershinin <ciapnz@gmail.com>
2026-05-28 00:08:33 +0700

    Fix block-ip: add CGI shebang, correct sudo path, fix IPv6 detection, configurable trusted IPs
    - block-ip.cgi was missing its #!/bin/bash shebang (fcgiwrap could not exec it)
      and called /usr/local/libexec/block-ip.sh while the script lives under sbin/;
      point it at /usr/local/sbin/block-ip.sh.
    - block-ip.sh keyed IPv6 vs IPv4 off $1, which is empty when invoked via the CGI
      (REMOTE_ADDR comes from the environment), so v6 hits never reached honeypot6;
      detect family by the presence of a colon instead.
    - Lift the hard-coded TRUSTED_IPS into an optional /etc/nginx/honeypot/trusted-ips.conf
      so installs edit config, not the script; default to 127.0.0.1 and ::1.

--- libexec/block-ip.cgi
+++ libexec/block-ip.cgi
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "Content-type: text/plain"
+
 echo "Status: 410 Gone"
+echo "Content-type: text/plain"
 echo "Connection: close"
 echo
 
 echo "Bye bye, $REMOTE_ADDR!"
-sudo /usr/local/libexec/block-ip.sh
+sudo /usr/local/sbin/block-ip.sh
 
 exit 0
--- sbin/block-ip.sh
+++ sbin/block-ip.sh
@@ -9,8 +9,16 @@ if [[ -z ${REMOTE_ADDR} ]]; then
     fi
 fi
 
-# Put space separate list of trusted IP addresses, not to lock yourself out if you like to test the honeypot! : )
-TRUSTED_IPS=("1.2.3.4" "2.3.4.5" "127.0.0.1")
+# Trusted IPs that must never be banned (so you don't lock yourself out while
+# testing). Override the defaults by editing /etc/nginx/honeypot/trusted-ips.conf
+# which simply redefines the TRUSTED_IPS array, e.g.:
+#   TRUSTED_IPS=("127.0.0.1" "::1" "203.0.113.7")
+TRUSTED_IPS=("127.0.0.1" "::1")
+if [[ -f /etc/nginx/honeypot/trusted-ips.conf ]]; then
+    # shellcheck disable=SC1091
+    source /etc/nginx/honeypot/trusted-ips.conf
+fi
+
 for ip in "${TRUSTED_IPS[@]}"; do
     if [[ "$REMOTE_ADDR" == "$ip" ]]; then
         echo "Trusted IP"
@@ -18,12 +26,11 @@ for ip in "${TRUSTED_IPS[@]}"; do
     fi
 done
 
-if [[ "$REMOTE_ADDR" != "${1#*[0-9].[0-9]}" ]]; then
-  /sbin/ipset add honeypot4 "${REMOTE_ADDR}"
-  /sbin/conntrack -D -s "${REMOTE_ADDR}"
-elif [[ "$REMOTE_ADDR" != "${1#*:[0-9a-fA-F]}" ]]; then
-  /sbin/ipset add honeypot6 "${REMOTE_ADDR}"
-  /sbin/conntrack -D -s "${REMOTE_ADDR}"
+# IPv6 addresses contain a colon; everything else is treated as IPv4.
+if [[ "$REMOTE_ADDR" == *:* ]]; then
+    /sbin/ipset add honeypot6 "${REMOTE_ADDR}"
+    /sbin/conntrack -D -s "${REMOTE_ADDR}"
 else
-  echo "Unrecognized IP format '$1'"
+    /sbin/ipset add honeypot4 "${REMOTE_ADDR}"
+    /sbin/conntrack -D -s "${REMOTE_ADDR}"
 fi
